8e8b0115ba7a6aa1a61a9f6f7270f708afda46e9
Abhinav Goel <abhinavgo1000@gmail.com>
2025-06-01 09:09:02 -0600

    auth model defined

--- ag_social_api/models.py
+++ ag_social_api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class UserAuth(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    dob = models.DateTimeField()
+
+    def __str__(self):
+        return self.first_name
+
